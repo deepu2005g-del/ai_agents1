7ac12c6720ddc8e377120374a1b914a2b5c3982f
deepu2005g-del <deepu2005g@gmail.com>
2026-02-10 11:03:21 +0530

    push

--- email_sender.py
+++ email_sender.py
@@ -3,15 +3,19 @@ from email.message import EmailMessage
 
 from secrets import EMAIL_ADDRESS, RECEIVER_EMAIL, EMAIL_PASSWORD
 
-msg = EmailMessage()
-msg["From"] = EMAIL_ADDRESS
-msg["To"] = RECEIVER_EMAIL
-msg["Subject"] = "Hello from Python 🐍"
+def send_email(RECEIVER_EMAIL: str, subject: str, content: str) -> str:
+    """ Send an email to the specified receiver."""
+    msg = EmailMessage()
+    msg["From"] = EMAIL_ADDRESS
+    msg["To"] = RECEIVER_EMAIL
+    msg["Subject"] = "Hello from Python 🐍"
 
-msg.set_content("The email was sent using Python!")
+    msg.set_content(content)
 
-with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-    server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-    server.send_message(msg)
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        server.send_message(msg)
 
-print("Email sent successfully!")
+    print("Email sent successfully!")
+    return "Email sent successfully!"
+
